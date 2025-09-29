b457e6e37e01d2d01664762e8e61b50f29696a13
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-09-29 23:21:34 +0800

    Update nomenclature_model.py

--- src/models/nomenclature_model.py
+++ src/models/nomenclature_model.py
@@ -1,3 +1,6 @@
+"""
+Модель номенклатуры
+"""
 
 from src.core.abstract_model import abstract_reference
 from src.core.validator import validator, argument_exception
@@ -8,34 +11,43 @@ class nomenclature_model(abstract_reference):
     __unit: 'unit_model' = None
 
     def __init__(self, name: str = ""):
+        """
+        Инициализация номенклатуры
+        Args:
+            name: наименование номенклатуры
+        """
         super().__init__(name)
         self.__full_name = name
 
+    """ Полное наименование (строка до 255 символов) """
     @property
     def full_name(self) -> str:
         return self.__full_name
 
+    """ Устанавливает полное наименование """
     @full_name.setter
     def full_name(self, value: str):
         validator.validate(value, str, 255)
-        self.__full_name = value
+        self.__full_name = value.strip()
 
+    """ Группа номенклатуры """
     @property
     def group(self):
         return self.__group
 
+    """ Устанавливает группу номенклатуры """
     @group.setter
     def group(self, value):
-        if value is not None and not isinstance(value, nomenclature_group_model):
-            raise argument_exception("Группа должна быть nomenclature_group_model")
+        validator.validate(value, (nomenclature_group_model, type(None)))
         self.__group = value
 
+    """ Единица измерения """
     @property
     def unit(self):
         return self.__unit
 
+    """ Устанавливает единицу измерения """
     @unit.setter
     def unit(self, value):
-        if value is not None and not isinstance(value, unit_model):
-            raise argument_exception("Единица должна быть unit_model")
+        validator.validate(value, (unit_model, type(None)))
         self.__unit = value
