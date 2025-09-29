edc69bf6ccfc00476c34f89b9c6fe5ece50c1279
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-09-29 23:23:25 +0800

    Update storage_model.py

--- src/models/storage_model.py
+++ src/models/storage_model.py
@@ -1,6 +1,16 @@
+"""
+Модель склада
+"""
+
 from src.core.validator import validator
 from src.core.abstract_model import abstract_reference
 
 class storage_model(abstract_reference):
+    
     def __init__(self, name: str = ""):
+        """
+        Инициализация модели склада
+        Args:
+            name: наименование склада
+        """
         super().__init__(name)
