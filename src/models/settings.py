b2e8f99c93a3dee177ddba938068d44c901407b6
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-09-29 23:23:53 +0800

    Update settings.py

--- src/models/settings.py
+++ src/models/settings.py
@@ -1,3 +1,7 @@
+"""
+Модель настроек приложения
+"""
+
 from src.models.company_model import company_model
 from src.core.validator import validator
 
@@ -7,10 +11,12 @@ class Settings:
     def __init__(self):
         self.__company = company_model()
 
+    """ Текущая организация """
     @property
-    def company(self):
+    def company(self) -> company_model:
         return self.__company
-
+    
+    """ Устанавливает текущую организацию """
     @company.setter
     def company(self, value: company_model):
         validator.validate(value, company_model)
