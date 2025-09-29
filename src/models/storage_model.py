ccde18afb39d0df9a975735d2081e93c46f3ac05
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-09-29 20:38:37 +0800

    Update storage_model.py

--- src/models/storage_model.py
+++ src/models/storage_model.py
@@ -1,1 +1,6 @@
+from src.core.validator import validator
+from src.core.abstract_model import abstract_reference
 
+class storage_model(abstract_reference):
+    def __init__(self, name: str = ""):
+        super().__init__(name)
