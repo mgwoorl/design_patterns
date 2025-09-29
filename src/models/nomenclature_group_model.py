faf01cde856a90315ef44facaaa53478787ec063
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-09-29 20:37:30 +0800

    Update nomenclature_group_model.py

--- src/models/nomenclature_group_model.py
+++ src/models/nomenclature_group_model.py
@@ -1,1 +1,5 @@
+from src.core.abstract_model import abstract_reference
 
+class nomenclature_group_model(abstract_reference):
+    def __init__(self, name: str = ""):
+        super().__init__(name)
