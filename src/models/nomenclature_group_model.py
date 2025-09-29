608e95fdebdb8481c968c48f2cbcf469cedb79d2
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-09-29 23:21:12 +0800

    Update nomenclature_group_model.py

--- src/models/nomenclature_group_model.py
+++ src/models/nomenclature_group_model.py
@@ -1,5 +1,15 @@
+"""
+Модель группы номенклатуры
+"""
+
 from src.core.abstract_model import abstract_reference
 
 class nomenclature_group_model(abstract_reference):
+    
     def __init__(self, name: str = ""):
+        """
+        Инициализация группы номенклатуры
+        Args:
+            name: наименование группы
+        """
         super().__init__(name)
