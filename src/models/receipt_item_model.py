d9ddc278f9b74168814d3549f72312c335b8f559
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-10-20 20:00:55 +0800

    Create receipt_item_model.py

---
+++ src/models/receipt_item_model.py
@@ -0,0 +1,16 @@
+from src.core.abstract_model import abstact_model
+from src.models.nomenclature_model import nomenclature_model
+from src.models.range_model import range_model
+
+# Модель элемента рецепта
+class receipt_item_model(abstact_model):
+    __nomenclature:nomenclature_model
+    __range:range_model
+    __value:int
+
+    # Фабричный метод
+    def create(nomenclature:nomenclature_model, range:range_model,  value:int):
+        item = receipt_item_model()
+        item.__nomenclature = nomenclature
+        item.__range = range
+        item.__value = value
