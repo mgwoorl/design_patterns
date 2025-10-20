2bff79995533e24f9ab6a014e175e17b43e06278
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-10-20 19:49:53 +0800

    Update abstract_model.py

--- src/core/abstract_model.py
+++ src/core/abstract_model.py
@@ -1,58 +1,36 @@
-"""
-Базовый абстрактный класс для справочных сущностей
-"""
-
 from abc import ABC
 import uuid
-from src.core.validator import validator, argument_exception
+from src.core.validator import validator
 
 """
-Базовый абстрактный класс модели
-Содержит уникальный код и перегрузку сравнения
+Абстрактный класс для наследования моделей
+Содержит в себе только генерацию уникального кода
 """
-class abstract_reference(ABC):
+class abstact_model(ABC):
+    __unique_code:str
 
-    __id: str
-    __name: str = ""
-
-    def __init__(self, name: str = "") -> None:
-        """
-        Инициализация справочной модели
-        Args:
-            name: наименование (строка до 50 символов)
-        """
+    def __init__(self) -> None:
         super().__init__()
-        self.__id = uuid.uuid4().hex
-        if name:
-            self.name = name
+        self.__unique_code = uuid.uuid4().hex
 
-    """ Уникальный код модели """
+    """
+    Уникальный код
+    """
     @property
-    def id(self) -> str:
-        return self.__id
-
-    """ Устанавливает уникальный код """
-    @id.setter
-    def id(self, value: str):
+    def unique_code(self) -> str:
+        return self.__unique_code
+    
+    @unique_code.setter
+    def unique_code(self, value: str):
         validator.validate(value, str)
-        self.__id = value.strip()
+        self.__unique_code = value.strip()
+    
 
-    """ Наименование (строка до 50 символов) """
-    @property
-    def name(self) -> str:
-        return self.__name
+    """
+    Перегрузка штатного варианта сравнения
+    """
+    def __eq__(self, value) -> bool:
+        if value is  None: return False
+        if not isinstance(value, abstact_model): return False
 
-    """ Устанавливает наименование с проверкой длины (<=50) """
-    @name.setter
-    def name(self, value: str):
-        try:
-            validator.validate(value, str, 50)
-        except argument_exception as ex:
-            raise
-        self.__name = value.strip()
-
-    """ Перегрузка сравнения по уникальному коду """
-    def __eq__(self, other) -> bool:
-        if isinstance(other, abstract_reference):
-            return self.__id == other.__id
-        return False
+        return self.unique_code == value.unique_code
