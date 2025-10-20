90c80ab308713c70f4c6b4b5c120a39d6dc753c4
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-10-20 19:50:07 +0800

    Update abstract_response.py

--- src/core/abstract_response.py
+++ src/core/abstract_response.py
@@ -1,1 +1,15 @@
+import abc
+from src.core.validator import validator, operation_exception
 
+# Абстрактный класс для формирования ответов
+class abstract_response(abc.ABC):
+    
+    # Сформировать нужный ответ
+    @abc.abstractmethod
+    def build(self, data: list) -> str:
+        validator.validate(data, list)
+
+        if len(data) == 0:
+            raise operation_exception("Нет данных!")
+
+        return ""
